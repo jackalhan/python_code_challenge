cac27097909f00a8762cf0c7617d7fc9fb8ef945
Tolgahan Cakaloglu <jackalhan@users.noreply.github.com>
2017-02-22 00:13:16 -0600

    setup question

---
+++ command_parser.py
@@ -0,0 +1,13 @@
+"""
+Handles the work of validating and processing command input.
+"""
+
+
+def get_valid_commands(queue, fi):
+    # TODO: efficiently evaluate commands
+    queue.put(command)
+
+
+def process_command_output(queue):
+    # TODO: run the command and put its data in the db
+    command = queue.get()
